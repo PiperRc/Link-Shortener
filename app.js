d4c5f1369b0d922fafb41ce682441b10dd5def59
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-03-08 21:22:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const hamburger = document.querySelector('.navbar-toggler');
-const s = document.querySelector('main section:nth-child(2)')
-const heroImg = document.querySelector('.hero-img');
-// const header = document.querySelector('header h1')
+const hamburger = document.querySelector('.hamburger');
+const heroImg = document.querySelector('main section:nth-child(2)');
 
 hamburger.addEventListener('click', () => {
-    if (s.classList.contains("hero-img-removed")) {
-        s.classList.remove("hero-img-removed");
-    } else {
-        s.classList.add("hero-img-removed");
-    }
+    heroImg.classList.toggle('hero-img-removed');
+
 
 })
